cd874194567663c181f7f5ab0316f6f843a4a3ce
Szymczek <szymon.szymczewski@protonmail.com>
2021-05-03 19:34:03 +0200

    new comments

--- operator=_example/operator=_example.cpp
+++ operator=_example/operator=_example.cpp
@@ -6,13 +6,15 @@ using namespace std;
 int main()
 {
     int size1, size2;
-    cout << "Matrix" << endl;
+
     cout << "Wprowadz rozmiar macierzy 1: " << endl;
     cin >> size1;
     Matrix matrix1(size1, size1);
+
     cout << "Wprowadz rozmiar macierzy 2: " << endl;
     cin >> size2;
     Matrix matrix2(size2, size2);
+
     cout << "Matrix1" << endl;
     matrix1.showMatrix();
     cout << "Matrix2" << endl;
